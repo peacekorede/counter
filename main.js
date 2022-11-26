d7c4db35ca61949cc0a4e1ee7fdce3b7b6978243
Nielvid <56310246+nielvid@users.noreply.github.com>
2022-11-26 19:17:43 +0100

    improved

--- main.js
+++ main.js
@@ -2,6 +2,33 @@ let countEl = document.getElementById('count_el')
 let total = document.getElementById('passengers')
 let additional = document.getElementById('additional')
 
+let btn = document.getElementById('increment_btn')
+
+
+let input = document.getElementById('input')
+let btn_2 = document.getElementById('btn_2')
+let display = document.getElementById('display')
+
+
+let todo = []
+btn_2.addEventListener('click', () => {
+    console.log(input.value)
+    todo.push(input.value)
+ display.innerHTML += input.value + '<br />'
+input.value = ''
+console.log(todo)
+})
+
+let arr = [1,3,4,5,6,6,7,78,]
+arr.forEach((element) => {
+  console.log(element)
+})
+
+
+// for( x=0; x < todo.length; x++){
+
+// }
+
 let count = 0
 function increment() {
     count = count + 1
@@ -29,5 +56,3 @@ countEl.innerHTML = count
  console.log(database, `record of passengers as at ${new Date().getMinutes}`)   
 }
 
-    
-    
