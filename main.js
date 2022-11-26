0079149fcfb028fda9dc5dfb8c6c2cef9bf49dd4
Nielvid <56310246+nielvid@users.noreply.github.com>
2022-11-26 15:32:07 +0100

    added save functionality

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 
-
 let count = 0
+
+let countEl = document.getElementById('count_el')
 
 function increment() {
     count = count + 1
     countEl.innerHTML = count
 }
 
-let countEl = document.getElementById("count_el")
 
+let database = []
 
 function save() {
-     console.log(count)    
+ 
+// 1. Save the value of counter and the current time as an object inside the database
+ // 2. Print out the difference between the last value of database and the current value
+let lastDbRecord = database[database.length - 1]
+let last = lastDbRecord?.numberOfPassenger ||  0
+ const data = {
+    numberOfPassenger: count,
+    time: new Date(),
+    additionalPassengers: count - last
 }
-    
+database.push(data)
+
+ console.log(database, `record of passengers as at ${new Date().getMinutes}`)   
+}
+
     
     
